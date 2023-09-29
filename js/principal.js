f09bd913008a07f2993b32ad83109d35c3bae77d
Antony <109869237+antony270707@users.noreply.github.com>
2023-09-29 09:06:29 -0300

    Add files via upload

---
+++ js/principal.js
@@ -0,0 +1,38 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    
+    if(peso <= 0 || peso >= 699){
+        console.log("peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido!";
+        paciente.style.color = "red"
+    }
+    
+    if(altura <= 0 || altura >= 3.00){
+        console.log("altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Inválida";
+    }
+    
+    if( alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
